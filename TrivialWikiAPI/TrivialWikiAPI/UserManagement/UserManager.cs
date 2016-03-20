6ced9f4f2ce91ddf8c9fecf7b2901f0a1218ceba
Pasca Ionut <pascaionut94@yahoo.com>
2016-03-20 13:44:41 +0200

    Added some CRUD functionalities for the user

--- TrivialWikiAPI/TrivialWikiAPI/UserManagement/UserManager.cs
+++ TrivialWikiAPI/TrivialWikiAPI/UserManagement/UserManager.cs
@@ -32,6 +32,91 @@ namespace TrivialWikiAPI.UserManagement
             }
         }
 
+        public void ChangeUserPassword(User user)
+        {
+            using (var databaseContext = new DatabaseContext())
+            {
+                var usr = databaseContext.Users.Single(u => u.UserName == user.UserName);
+                if (usr == null)
+                {
+                    return;
+                }
+                var password = Encrypt.GetMD5(user.Password);
+                usr.Password = password;
+                databaseContext.SaveChanges();
+            }
+        }
+
+        public void RemoveUserFromDatabase(string userName)
+        {
+            using (var databaseContext = new DatabaseContext())
+            {
+                var user = databaseContext.Users.Single(u => u.UserName == userName);
+                databaseContext.Users.Remove(user);
+                databaseContext.SaveChanges();
+            }
+        }
+
+        public void AddPointsToUser(string userName, int points)
+        {
+            using (var databaseContext = new DatabaseContext())
+            {
+                var user = databaseContext.Users.Single(u => u.UserName == userName);
+
+                if (points > 0)
+                {
+                    IncrementUserRank(points, databaseContext, user);
+                }
+                else
+                {
+                    DecrementUserRank(points, databaseContext, user);
+                }
+
+                databaseContext.SaveChanges();
+            }
+        }
+        private static void IncrementUserRank(int points, DatabaseContext databaseContext, User user)
+        {
+            var usersWithRankDepreciated = databaseContext.Users
+                .Where(u => u.Points > user.Points && u.Points < user.Points + points)
+                .ToList();
+            if (usersWithRankDepreciated.Count == 0)
+            {
+                return;
+            }
+            usersWithRankDepreciated.ForEach(u => u.Rank = u.Rank - 1);
+
+            user.Points = user.Points + points;
+            user.Rank = user.Rank - usersWithRankDepreciated.Count;
+        }
+
+        private static void DecrementUserRank(int points, DatabaseContext databaseContext, User user)
+        {
+            var usersToIncreaseRank = databaseContext.Users
+                .Where(u => u.Points < user.Points && u.Points > user.Points + points)
+                .ToList();
+            if (usersToIncreaseRank.Count == 0)
+            {
+                return;
+            }
+            usersToIncreaseRank.ForEach(u => u.Rank = u.Rank + 1);
+
+            user.Points = user.Points + points;
+            user.Rank = user.Rank - usersToIncreaseRank.Count;
+        }
+
+        public void ChangeUserRole(string userName, int roleId)
+        {
+            using (var databaseContext = new DatabaseContext())
+            {
+                var user = databaseContext.Users.Single(u => u.UserName == userName);
+                var role = databaseContext.Roles.Single(r => r.Id == roleId);
+                user.Roles.Clear();
+                user.Roles.Add(role);
+                databaseContext.SaveChanges();
+            }
+        }
+
         public bool UserExists(string userName)
         {
             using (var databaseContext = new DatabaseContext())
@@ -39,5 +124,14 @@ namespace TrivialWikiAPI.UserManagement
                 return databaseContext.Users.Any(u => u.UserName == userName);
             }
         }
+
+        public bool RoleExists(int roleId)
+        {
+            using (var databaseContext = new DatabaseContext())
+            {
+                return databaseContext.Roles.Any(r => r.Id == roleId);
+            }
+        }
+
     }
 }
