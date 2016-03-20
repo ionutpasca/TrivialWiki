d8f3c6730f49a4ecfd15e9c5f19d7374b8b4ccfb
Pasca Ionut <pascaionut94@yahoo.com>
2016-03-20 12:55:06 +0200

    Add new user functionality (Backend)

--- TrivialWikiAPI/TrivialWikiAPI/UserManagement/UserManager.cs
+++ TrivialWikiAPI/TrivialWikiAPI/UserManagement/UserManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using TrivialWikiAPI.DatabaseModels;
+using TrivialWikiAPI.Utilities;
 
 namespace TrivialWikiAPI.UserManagement
 {
@@ -11,6 +12,31 @@ namespace TrivialWikiAPI.UserManagement
             using (var databaseContext = new DatabaseContext())
             {
                 return databaseContext.Users.ToList();
+            }
+        }
+
+        public void AddNewUserToDatabase(User user)
+        {
+            user.Points = 0;
+            user.Password = Encrypt.GetMD5(user.Password);
+
+            using (var databaseContext = new DatabaseContext())
+            {
+                user.Rank = databaseContext.Users.Count();
+
+                var playerRole = databaseContext.Roles.FirstOrDefault(r => r.Name == "Player");
+                user.Roles.Add(playerRole);
+
+                databaseContext.Users.Add(user);
+                databaseContext.SaveChanges();
+            }
+        }
+
+        public bool UserExists(string userName)
+        {
+            using (var databaseContext = new DatabaseContext())
+            {
+                return databaseContext.Users.Any(u => u.UserName == userName);
             }
         }
     }
--- TrivialWikiAPI/TrivialWikiAPI/UserManagement/UserModule.cs
+++ TrivialWikiAPI/TrivialWikiAPI/UserManagement/UserModule.cs
@@ -1,9 +1,35 @@
 ﻿using Nancy;
+using Nancy.ModelBinding;
+using TrivialWikiAPI.DatabaseModels;
+using TrivialWikiAPI.Utilities;
 
 namespace TrivialWikiAPI.UserManagement
 {
     public class UserModule : NancyModule
     {
         private readonly UserManager userManager = new UserManager();
+
+        public UserModule()
+        {
+            Post["/addNewUser"] = param => AddNewUserToDatabase();
+        }
+
+        private dynamic AddNewUserToDatabase()
+        {
+            var user = this.Bind<User>();
+            if (string.IsNullOrEmpty(user?.UserName) || string.IsNullOrEmpty(user.Password))
+            {
+                return HttpStatusCode.BadRequest;
+            }
+
+            var userExists = userManager.UserExists(user.UserName);
+            if (userExists)
+            {
+                return HttpStatusCode.Conflict;
+            }
+
+            userManager.AddNewUserToDatabase(user);
+            return HttpStatusCode.OK;
+        }
     }
 }
