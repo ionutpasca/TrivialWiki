02f95e6e7e34945faa45feab004dc056f2ba13aa
Pasca Ionut <pascaionut94@yahoo.com>
2016-05-03 18:13:39 +0300

    Added owin app to host the API

---
+++ TrivialWikiAPI/TrivialWikiAPI.Host/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace TrivialWikiAPI.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:4606/";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine($"Running on {url}");
+                Console.WriteLine("Press enter to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ TrivialWikiAPI/TrivialWikiAPI.Host/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace TrivialWikiAPI.Host
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
